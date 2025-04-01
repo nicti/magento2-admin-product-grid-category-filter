db8ca4e8cc39e7677ef4283efaa076225881df53
Niclas Titius <niclas.titius@nicti.de>
2025-04-01 14:03:29 +0200

    Update CategoryList.php

--- Model/Category/CategoryList.php
+++ Model/Category/CategoryList.php
@@ -14,19 +14,32 @@ class CategoryList implements ArrayInterface
     ) {
         $this->_categoryCollectionFactory = $collectionFactory;
     }
-    
+
     public function toOptionArray($addEmpty = true)
     {
-        $categoryCollection = $this->_categoryCollectionFactory->create()->addAttributeToSelect('name')->setOrder('name', 'ASC');
+        $categoryCollection = $this->_categoryCollectionFactory->create()->addAttributeToSelect('name')->setOrder('path', 'ASC');
 
         $options = [];
+        $nameMap = [];
 
         if ($addEmpty) {
             $options[] = ['label' => __('-- Please Select a Category --'), 'value' => ''];
         }
 
         foreach ($categoryCollection as $category) {
-            $options[] = ['label' => $category->getName(), 'value' => $category->getId()];
+            if ($category->getId() == 1) continue;
+            $nameMap[$category->getId()] = $category->getName();
+            if (str_contains($category->getPath(), '/')) {
+                $ids = explode('/', $category->getPath());
+                $names = [];
+                foreach ($ids as $id) {
+                    if ($id == 1) continue;
+                    $names[] = $nameMap[$id]??'';
+                }
+                $options[] = ['label' => implode(' > ', $names), 'value' => $category->getId()];
+            } else {
+                $options[] = ['label' => $category->getName(), 'value' => $category->getId()];
+            }
         }
 
         return $options;
